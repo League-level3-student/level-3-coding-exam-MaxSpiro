c265b5ea8cdb347d81e6e8ff9f8c0fccbde807a2
League Student <league@iMac-37.attlocal.net>
2020-01-19 14:26:46 -0800

    small edits

--- src/VoteProcessor.java
+++ src/VoteProcessor.java
@@ -4,12 +4,14 @@ public class VoteProcessor {
 	public String calculateElectionWinner(ArrayList<String> a) {
 		int francis=0;
 		int snowden=0;
+		
 		for(int i=0; i< a.size();i++) {
 			if(a.get(i).equalsIgnoreCase("pope francis"))
 				francis++;
 			else
 				snowden++;
 		}
+		
 		if(francis>snowden) 
 			return "pope francis";
 		else if(snowden>francis)
--- test/HashMapCalculator.java
+++ test/HashMapCalculator.java
@@ -3,6 +3,7 @@ import java.util.HashMap;
 public class HashMapCalculator {
 	public int commonKeyValuePairs(HashMap<String, String> a, HashMap<String, String> b){
 		int c = 0;
+		
 		for(String s : a.keySet()) {
 			for(String t : b.keySet()) {
 				if((s.equalsIgnoreCase(t))&&(a.get(s).equalsIgnoreCase(b.get(t))))
