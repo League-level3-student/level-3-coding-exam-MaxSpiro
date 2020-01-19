2b560415d186270ccb440f7a1654377eed9a171f
League Student <league@iMac-37.attlocal.net>
2020-01-19 14:17:41 -0800

    finished and passed coding exam

--- src/VoteProcessor.java
+++ src/VoteProcessor.java
@@ -1,4 +1,20 @@
+import java.util.ArrayList;
 
 public class VoteProcessor {
-
+	public String calculateElectionWinner(ArrayList<String> a) {
+		int francis=0;
+		int snowden=0;
+		for(int i=0; i< a.size();i++) {
+			if(a.get(i).equalsIgnoreCase("pope francis"))
+				francis++;
+			else
+				snowden++;
+		}
+		if(francis>snowden) 
+			return "pope francis";
+		else if(snowden>francis)
+			return "edward snowden";
+		else 
+			return "TIE";
+	}
 }
